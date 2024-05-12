c6e22e6b3ec96576df88741247288a681486d91e
SealtielFreak <Die.Seal_Valderrama@outlook.com>
2024-05-12 13:30:18 -0600

    Minor changes

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+DEFAULT_NAME = "tree_search_algorithm"
+DEFAULT_VERSION = "0.0.1"
+DEFAULT_DESCRIPTION = "Structure for work with tree search algorithm"
+
+setup(name=DEFAULT_NAME,
+      version=DEFAULT_VERSION,
+      description=DEFAULT_DESCRIPTION,
+      url='https://github.com/SealtielFreak/tree_search_algorithm',
+      author='Diego Sealtiel Valderrama Garcia',
+      author_email='sealtielfreak@yandex.com',
+      license='MIT',
+      packages=['tree_structure'],
+      zip_safe=False
+      )
